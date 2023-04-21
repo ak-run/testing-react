f8ffa5bdc37995ca38a64eb705ff1da5e6d550fe
ak-run <123417735+ak-run@users.noreply.github.com>
2023-04-21 11:29:47 +0100

    learning React Hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import photo from './guitar.jpg';
-import { Button } from "./components/component";
+import { UseStateTutorial } from './components/UseStateHook';
+import { InputState } from './components/UseStateHook';
+import { ReducerTutorial } from './components/useReducerHook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>My Hobby: Playing the Guitar</h1>
-        <img src={photo} alt="photo not recognised" className="guitar"/>
-        <p>I started playing the guitar when I was 16. It helps me with my singing exercies. It helps me relax. It's easy to start but requires a lot of work to continue.</p>
-        <h4>Why do I like it?</h4>
-            <p className="list">It's creative</p>
-            <p className="list">It's fun</p>
-            <p className="list">It helps me relax</p>
-          <h4>Helpful resources</h4>
-            <p className="list" >
-              <a href="https://en.wikipedia.org/wiki/Guitar" target="_blank"><Button label="Wikipedia page"></Button></a>
-            </p>
-            <p className="list">
-              <a href="https://www.fender.com/play" target="_blank"><Button label="Fender online lessons"></Button></a>
-            </p>
-        <p className="list"><a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </p>
+        <h1>Learning React</h1>
       </header>
+      <div className="learning">
+        <UseStateTutorial></UseStateTutorial>
+        <InputState></InputState>
+        <ReducerTutorial></ReducerTutorial>
+      </div>
     </div>
   );
 }
--- src/components/component.jsx
+++
@@ -1,7 +0,0 @@
-export const Button = (props) => {
-  const { label } = props;
-  return (
-    //to style components add className in the component, not in the App.js
-    <button className="button">{label}</button>  
-    )
-}
